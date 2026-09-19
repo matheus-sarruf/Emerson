7e450f52cc26e69bcdeb3d38d5d7245a9f6b33b7
matheus-sarruf <matheus.sarruf18@gmail.com>
2026-09-19 16:54:59 -0300

    3

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,9 @@ const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/auth');
 const upload = require('../middlewares/upload');
 
+// Rota de login (adicione esta linha)
+router.post('/login', authController.login);
+
 router.put('/profile', authMiddleware, authController.updateProfile);
 router.post('/profile/image', authMiddleware, upload.single('profileImage'), authController.uploadProfileImage);
 
